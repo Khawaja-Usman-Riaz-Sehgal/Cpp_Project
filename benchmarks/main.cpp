bd20afe3e47a8e6cbde97f41071317f63303ddfa
Khawaja Usman Riaz Sehgal <usman.riaz@emumba.com>
2020-09-28 12:53:52 +0500

    Benchmarking my code

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,18 +1,43 @@
+#include <Containers.h>
 #include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
+using namespace std;
+
+Containers vector_container;
+Containers list_container;
+
+static void BM_VECTOR(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        vector<int> vector_test(state.range(0));
+        generate(vector_test.begin(), vector_test.end(), rand);
+        vector_container.set_sum(vector_test);
+    }
 }
-BENCHMARK(BM_StringCopy);
+
+static void BM_LIST(benchmark::State& state)
+{
+    for(auto _ : state)
+    {
+        list<int> list_test(state.range(0));
+        generate(list_test.begin(), list_test.end(), rand);
+    }
+}
+
+static void BM_VECTOR_Million(benchmark::State& state)
+{
+     for (auto _ : state)
+    {
+        vector<int> vector_test(1000000);
+        generate(vector_test.begin(), vector_test.end(), rand);
+        vector_container.set_sum(vector_test);
+    }
+}
+
+BENCHMARK(BM_VECTOR) -> Arg(1000) -> Arg(5000) -> Arg(10000);
+BENCHMARK(BM_LIST) -> Arg(1000) -> Arg(5000) -> Arg(10000);
+BENCHMARK(BM_VECTOR_Million);
 
 BENCHMARK_MAIN();
